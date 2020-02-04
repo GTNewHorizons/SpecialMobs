dac1552ba63d15dd4886b3093155a539b02c2c6e
Richard Hendricks <richardhendricks@pobox.com>
2020-02-03 21:56:46 -0600

    Jolts only teleport now when hit by a projectile. When hit, they take 1/5th the damage from projectiles. This has no minimum, so if you have a weak arrow, it takes 0 damage just like before. Melee does not teleport the Jolt, but it does still cause a lightning strike.

--- src/main/java/toast/specialMobs/entity/blaze/EntityJoltBlaze.java
+++ src/main/java/toast/specialMobs/entity/blaze/EntityJoltBlaze.java
@@ -1,13 +1,18 @@
 package toast.specialMobs.entity.blaze;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.init.Items;
+import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.EntityDamageSourceIndirect;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.IChatComponent;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import toast.specialMobs.MobHelper;
 import toast.specialMobs._SpecialMobs;
+import toast.specialMobs.EventHandler.DamageSourcePain;
 
 public class EntityJoltBlaze extends Entity_SpecialBlaze
 {
@@ -51,24 +56,27 @@ public class EntityJoltBlaze extends Entity_SpecialBlaze
             double zI = this.posZ;
 
             for (int i = 0; i < 16; i++) {
-			    if (this.teleportRandomly()) {
-			    	if (damageSource instanceof EntityDamageSourceIndirect)
-			    		return true;
-		    		boolean hit = super.attackEntityFrom(damageSource, damage);
+            	// Reworking Jolt logic.  
+            	// Jolt will take damage/5 & teleport if hit by indirect damage
+            	// Jolt will not teleport from other damage sources
+            	if (damageSource instanceof EntityDamageSourceIndirect) {
+            		damage = damage/5;
+            		this.teleportRandomly();
+            	}
+            	boolean hit = super.attackEntityFrom(damageSource, damage);
 
-			    	if (this.getHealth() > 0.0F) {
-			        	MobHelper.lightningExplode(this, xI, yI, zI, 0);
-			    	}
-			    	else {
-			    		this.setPosition(xI, yI, zI);
-			    	}
-			        return hit;
-			    }
+		    	if (this.getHealth() > 0.0F) {
+		        	MobHelper.lightningExplode(this, xI, yI, zI, 0);
+		    	}
+		    	else {
+		    		this.setPosition(xI, yI, zI);
+		    	}
+		        return hit;
 			}
 		}
         return super.attackEntityFrom(damageSource, damage);
     }
-
+    
     /// Teleports this enderman to a random nearby location. Returns true if this entity teleports.
     protected boolean teleportRandomly() {
         double x = this.posX + (this.rand.nextDouble() - 0.5) * 16.0;
