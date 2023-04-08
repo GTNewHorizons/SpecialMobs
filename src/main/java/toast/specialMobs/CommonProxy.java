dd4a3ed3ecee0ecbf5eb7af9dbb5c2b60a83c9ca
OvermindDL1 <overminddl1@gmail.com>
2023-04-08 14:36:57 -0600

    Message discriminators *MUST* exist on the channel on both sides if t… (#18)
    * Message discriminators *MUST* exist on the channel on both sides if there is more than one message or the one message does not use ID 0, and this mod has two message, meaning when a MessageTexture gets sent from the server then it gets a discriminant of 0, meaning the client then deserializes it as MessageExplosion, which "most" of the time it will just harmless tell the client explosions are happening randomly around the world where they are not, but sometimes it crashes due to not enough data in the packet stream to deserialize, registering the messages on both server and client fixes it
    
    * The mod was missing some sidedness checks, why.... fixed

--- src/main/java/toast/specialMobs/CommonProxy.java
+++ src/main/java/toast/specialMobs/CommonProxy.java
@@ -1,9 +1,15 @@
 package toast.specialMobs;
 
+import net.minecraft.world.World;
+
 public class CommonProxy {
 
     // Registers render files if this is the client side.
     public void registerRenderers() {
         // Client method.
+    }
+
+    public World getClientWorld() {
+        return null;
     }
 }
