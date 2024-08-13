fcf8f97fe0633dcacbcb0959fc10fa1dbcecfe4d
kuba6000 <kuba.123123.6000@gmail.com>
2024-08-13 02:17:28 +0200

    1.62s + 2s

--- src/main/java/toast/specialMobs/entity/cavespider/EntityMotherCaveSpider.java
+++ src/main/java/toast/specialMobs/entity/cavespider/EntityMotherCaveSpider.java
@@ -1,5 +1,9 @@
 package toast.specialMobs.entity.cavespider;
 
+import java.util.ArrayList;
+
+import javax.annotation.Nonnull;
+
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.EntityList;
 import net.minecraft.entity.IEntityLivingData;
@@ -12,11 +16,16 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 
+import com.kuba6000.mobsinfo.api.IMobInfoProvider;
+import com.kuba6000.mobsinfo.api.MobDrop;
+
+import cpw.mods.fml.common.Optional;
 import toast.specialMobs.EffectHelper;
 import toast.specialMobs._SpecialMobs;
 import toast.specialMobs.entity.SpecialMobData;
 
-public class EntityMotherCaveSpider extends Entity_SpecialCaveSpider {
+@Optional.Interface(iface = "com.kuba6000.mobsinfo.api.IMobInfoProvider", modid = "mobsinfo")
+public class EntityMotherCaveSpider extends Entity_SpecialCaveSpider implements IMobInfoProvider {
 
     public static final ResourceLocation[] TEXTURES = new ResourceLocation[] {
             new ResourceLocation(_SpecialMobs.TEXTURE_PATH + "cavespider/mother.png"),
@@ -45,6 +54,7 @@ public class EntityMotherCaveSpider extends Entity_SpecialCaveSpider {
     /// Called when this entity is killed.
     @Override
     protected void dropFewItems(boolean hit, int looting) {
+        // ALL CHANGES IN HERE MUST BE ALSO MADE IN provideDropsInformation
         super.dropFewItems(hit, looting);
         if (hit && (this.rand.nextInt(3) == 0 || this.rand.nextInt(1 + looting) > 0)) {
             this.entityDropItem(
@@ -71,6 +81,7 @@ public class EntityMotherCaveSpider extends Entity_SpecialCaveSpider {
     /// Called 2.5% of the time when this entity is killed. 20% chance that superRare == 1, otherwise superRare == 0.
     @Override
     protected void dropRareDrop(int superRare) {
+        // ALL CHANGES IN HERE MUST BE ALSO MADE IN provideDropsInformation
         ItemStack itemStack;
         String name;
         if (this.rand.nextBoolean()) {
@@ -103,6 +114,50 @@ public class EntityMotherCaveSpider extends Entity_SpecialCaveSpider {
         EffectHelper.overrideEnchantment(itemStack, Enchantment.unbreaking, 10);
 
         this.entityDropItem(itemStack, 0.0F);
+    }
+
+    @Optional.Method(modid = "mobsinfo")
+    @Override
+    public void provideDropsInformation(@Nonnull ArrayList<MobDrop> drops) {
+        IMobInfoProvider.provideSuperVanillaDrops(drops, EntityCaveSpider.class);
+        drops.add(
+                MobDrop.create(new ItemStack(Items.spawn_egg, 1, 59 /* EntityCaveSpider */)).withChance(0.3333d)
+                        .withLooting());
+        final Item[] armor = { Items.chainmail_helmet, Items.chainmail_chestplate, Items.chainmail_leggings,
+                Items.chainmail_boots };
+        final String[] armorNames = { "Helmet", "Chestplate", "Leggings", "Boots" };
+        final Item[] tools = { Items.stone_sword, Items.bow, Items.stone_pickaxe, Items.stone_axe, Items.stone_shovel };
+        final String[] toolNames = { "Sword", "Bow", "Pickaxe", "Axe", "Shovel" };
+        double chance = 0.5d / armor.length;
+        for (int i = 0; i < armor.length; i++) {
+            Item item = armor[i];
+            int mindamage = 26;
+            int maxdamage = item.getMaxDamage() - 25;
+            if (mindamage > maxdamage) mindamage = maxdamage;
+            ItemStack stack = new ItemStack(item);
+
+            EffectHelper.setItemName(stack, "Partially Digested " + armorNames[i], 0xa);
+            EffectHelper.addItemText(stack, "\u00a77\u00a7oIt's a bit slimy...");
+
+            drops.add(
+                    MobDrop.create(stack).withType(MobDrop.DropType.Rare).withChance(0.025d * chance)
+                            .withRandomEnchant(30).withRandomDamage(mindamage, maxdamage));
+        }
+        chance = 0.5d / tools.length;
+        for (int i = 0; i < tools.length; i++) {
+            Item item = tools[i];
+            int mindamage = 26;
+            int maxdamage = item.getMaxDamage() - 25;
+            if (mindamage > maxdamage) mindamage = maxdamage;
+            ItemStack stack = new ItemStack(item);
+
+            EffectHelper.setItemName(stack, "Partially Digested " + toolNames[i], 0xa);
+            EffectHelper.addItemText(stack, "\u00a77\u00a7oIt's a bit slimy...");
+
+            drops.add(
+                    MobDrop.create(stack).withType(MobDrop.DropType.Rare).withChance(0.025d * chance)
+                            .withRandomEnchant(30).withRandomDamage(mindamage, maxdamage));
+        }
     }
 
     /// Saves this entity to NBT.
